c7351d8d6f77f12d8ab96a1b6eed3a91d7ff0efe
John Hartman <john.f.d.hartman@gmail.com>
2017-11-24 12:22:31 +1000

    fixed testing

--- public/qa/tests-about.js
+++ public/qa/tests-about.js
@@ -1,6 +1,8 @@
 
 suite('"about" Page Tests', function(){
 	test('page should contain link to contact page', function(){
-		assert($('a[href="/contact"]').length);
+		assert($('a').filter(function(){
+			return this.href.match(/contact$/);
+		}).length);
 	});
 });
