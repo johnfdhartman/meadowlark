12ca4fa0c34c0e9b61e698808882dba5d09e7bba
John Hartman <john.f.d.hartman@gmail.com>
2017-11-24 12:55:46 +1000

    finished what i needed from ch8

--- meadowlark.js
+++ meadowlark.js
@@ -38,6 +38,20 @@ app.get('/tours/request-group-rate', function(req, res){
 	res.render('tours/request-group-rate');
 });
 
+app.use(require('body-parser')());
+
+app.get('/newsletter', function(req, res) {
+	res.render('newsletter', { csrf: 'CSRF token goes here'});
+});
+
+app.post('/process', function(req, res){
+	console.log('Form (from querystring): ' + req.query.form); 
+	console.log('CSRF token (from hidden form field): ' + req.body._csrf); 
+	console.log('Name (from visible form field): ' + req.body.name); 
+	console.log('Email (from visible form field): ' + req.body.email); 
+	res.redirect(303, '/thank-you');
+});
+
 app.use(function(req,res){
 	res.status(404);
 	res.render('404');
