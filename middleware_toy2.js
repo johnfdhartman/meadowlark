86d3fd1346dd4e67df0cd24e7204f34a4298a3b9
John Hartman <john.f.d.hartman@gmail.com>
2017-11-24 15:14:21 +1000

    played with toy app

--- middleware_toy2.js
+++ middleware_toy2.js
@@ -1,6 +1,20 @@
 var app = require('express')();
 
-app.get('*', function(req, res){
+app.use(function(req,res,next){
+	console.log('middleware1');
+	next();
+});
+
+app.get('/a', function(req, res, next){
+	res.send('a');
+});
+
+app.use('/a',function(req, res, next){
+	console.log('middleware2');
+	next();
+});
+
+app.get('/', function(req, res){
 	console.log('root path');
 	res.send('yeaaa');
 });
